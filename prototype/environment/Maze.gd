2c6becf8c867a816b14c5b500111ae7462b9b557
EHIX <alexanderdowning@hotmail.com>
2021-02-21 15:02:06 +0000

    changed occlusion and collison of spritesheet, have basis of map generation

--- prototype/environment/Maze.gd
+++ prototype/environment/Maze.gd
@@ -12,7 +12,7 @@ var tile_size = 64  # tile size (in pixels)
 var width = 17  # width of map (in tiles)
 var height = 10  # height of map (in tiles)
 
-var map_seed = null
+var map_seed = 1234
 
 # fraction of walls to remove
 var erase_fraction = 0.2
