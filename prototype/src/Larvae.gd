17842221adee39fa247661ede954d8cc587ceac1
nmgs <neil.mcguiness@prevas.dk>
2021-04-04 14:56:46 +0200

    larvae collision detection and regular spawning dependent on number of current larvae in the world

--- prototype/src/Larvae.gd
+++ prototype/src/Larvae.gd
@@ -4,7 +4,7 @@ const TileTypes = preload("res://src/TileTypes.gd").Tile_Type
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
-var velocity = Vector2.ZERO
+#var velocity = Vector2.ZERO
 var current_dir = Vector2.UP # Default sprite is up
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,22 +26,26 @@ func _physics_process(delta: float) -> void:
 		
 func set_direction(new_direction: Vector2):
 	if new_direction == Vector2.UP:
-		velocity.y -= 1
+#		velocity.y -= 1
 		rotation_degrees = 0
 		current_dir = Vector2.UP
 	elif new_direction == Vector2.RIGHT:
-		velocity.x += 1
+#		velocity.x += 1
 		rotation_degrees = 90
 		current_dir = Vector2.RIGHT
 	elif new_direction == Vector2.DOWN:
-		velocity.y += 1
+#		velocity.y += 1
 		rotation_degrees = 180
 		current_dir = Vector2.DOWN
 	elif new_direction == Vector2.LEFT:
-		velocity.x -= 1
+#		velocity.x -= 1
 		rotation_degrees = 270
 		current_dir = Vector2.LEFT
 		
+func reverse_direction():
+	current_dir = current_dir * -1
+	rotation_degrees = (int(rotation_degrees) + 180) % 360
+
 
 func decide_orientation(tile_type: int):
 	print("decide orientation, tile type = ", tile_type)
@@ -56,14 +60,17 @@ func decide_orientation(tile_type: int):
 	
 func _on_Area2D_area_entered(area: Area2D) -> void:
 	print("Larvae hit something! - ", area.get_parent().name)
-	
 
 
 func _on_Area2D_body_entered(body: Node) -> void:
 	print("Larvae body entered something! - ", body.name)
-#	if "TileMap" in body.name:
-#		queue_free()
-#
-#	if "Enemy" in body.name:
-#		queue_free()
-	
+	if "Maze" in body.name:
+		reverse_direction()
+	if "MapBoundary" in body.name:
+		reverse_direction()
+	if "Enemy" in body.name:
+		queue_free()
+		
+	if "Player" in body.name:
+		queue_free()
+
--- prototype/src/World.gd
+++ prototype/src/World.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 const TileTypes = preload("res://src/TileTypes.gd").Tile_Type
-
-var init_timout = 3.0 # DUMMY TIMEOUT
+const creature_timeout_secs = 10
+const max_creatures = 3
+var creature_timeout = creature_timeout_secs
 var larvae = preload("res://src/Larvae.tscn")
 var num_larvae = 0
 onready var Maze = get_node("Maze")
@@ -11,24 +12,26 @@ func _ready() -> void:
 	print("enum example - T Left = ",TileTypes.T_LEFT)
 	pass # Replace with function body.
 	
-#func get_input():
-#	if Input.is_action_pressed("click"):
+func get_input():
+	if Input.is_action_pressed("click"):
+		var larvaeCount = get_tree().get_nodes_in_group("LARVAE_INSTS").size()
+		print(" LARVAE COUNT = ", larvaeCount)
 #		Maze.get_tiletype_from_worldpos(get_global_mouse_position())
-#
+
 
 func _process(delta: float) -> void:
-#	get_input()
-	if init_timout > 0:# DUMMY TIMEOUT
-		init_timout -= delta# DUMMY TIMEOUT
+	get_input()
+	if creature_timeout > 0:# DUMMY TIMEOUT
+		creature_timeout -= delta# DUMMY TIMEOUT
 	else:
-		init_timout = 3.0
-		if num_larvae < 2:
+		creature_timeout = creature_timeout_secs
+		if get_tree().get_nodes_in_group("LARVAE_INSTS").size() < max_creatures:
 			spawn_larvae()
 
 func spawn_larvae():
-	# DUMMY Larvae spawn
 	var larvae_instance = larvae.instance()
-	print("Spawn Larvae!")
+#	print("Spawn Larvae!")
+	# Find a valid tile to put a larvae into
 	var found_valid_tile = false
 	var cell_location = Vector2(0, 0)
 	while found_valid_tile == false:
@@ -38,13 +41,12 @@ func spawn_larvae():
 		print("Cell location, Cell type = ", cell_location," ", cell_type)
 		if cell_type != Maze.block_tile:
 			found_valid_tile = true
+	# Set position of the larvae to the valid tile location (middle of the tile)
 	larvae_instance.global_position = ((cell_location * Maze.tile_size) + (Maze.tile_size / 2))
+	# Discover tile type to decide which orientation the larvae should be in
 	var target_tiletype = Maze.get_tiletype_from_worldpos(larvae_instance.global_position)
 	larvae_instance.decide_orientation(target_tiletype)
+	# Add the larvae instance to the world
 	get_tree().get_root().add_child(larvae_instance)
-	increment_larvae()
-
-func decrement_larvae():
-	num_larvae -=1
-func increment_larvae():
-	num_larvae += 1
+	# Keeps track of number of Larvae instances
+	larvae_instance.add_to_group("LARVAE_INSTS") 
